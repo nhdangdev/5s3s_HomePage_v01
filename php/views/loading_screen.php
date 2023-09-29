75721a0af8217b6a8ed435eddc21cd8c9c1d554d
nguyenhoangtin <vietnamlive@hotmail.com>
2023-09-29 18:53:25 +0700

    Create php files from html

---
+++ php/views/loading_screen.php
@@ -0,0 +1,6 @@
+<header>
+    <div id="loading-screen">
+        <img src="../asset/img/logo.png" width="54" height="54" alt="Logo">
+        <div class="loader"></div>
+    </div>
+</header>
---
+++ php/views/sidebar.php
@@ -0,0 +1,73 @@
+<div class="sidebar sidebar--hidden">
+    <div class="sidebar__header">
+        <div class="account">
+            <div class="account__avatar">
+                <img src="https://picsum.photos/200" alt="Avatar">
+            </div>
+            <div class="account__name">User name</div>
+            <div class="account__actions">
+                <button>
+                    <span class="material-symbols-outlined">
+                        expand_more
+                    </span>
+                </button>
+            </div>
+        </div>
+        <button onclick="toggleSidebar()" class="sidebar__close-btn">
+            <span class="material-symbols-outlined">
+                close
+            </span>
+        </button>
+    </div>
+    <div class="sidebar__menu">
+        <div class="sidebar__item sidebar__item--selected">
+            <span class="material-symbols-outlined">
+                space_dashboard
+            </span>
+            <div class="sidebar-item__text">
+                Dashboard
+            </div>
+        </div>
+        <div class="sidebar__item">
+            <span class="material-symbols-outlined">
+                person
+            </span>
+            <div class="sidebar-item__text">
+                Users
+            </div>
+        </div>
+        <div class="sidebar__item">
+            <span class="material-symbols-outlined">
+                calendar_month
+            </span>
+            <div class="sidebar-item__text">
+                History
+            </div>
+        </div>
+        <div class="sidebar__item">
+            <span class="material-symbols-outlined">
+                flag
+            </span>
+            <div class="sidebar-item__text">
+                Report
+            </div>
+        </div>
+        <div class="sidebar__item">
+            <span class="material-symbols-outlined">
+                settings
+            </span>
+
+            <div class="sidebar-item__text">
+                Report
+            </div>
+        </div>
+    </div>
+    <div class="sidebar__footer">
+        <button id="logout-desktop-btn" style="margin-left: -.5rem;">
+            <span class="material-symbols-outlined">
+                move_item
+            </span>
+            <span>Log Out</span>
+        </button>
+    </div>
+</div>
