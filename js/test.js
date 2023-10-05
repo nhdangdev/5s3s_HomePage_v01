e4ba67614e32c05aae2fa2cf4f5874c053779956
nguyenhoangtin <vietnamlive@hotmail.com>
2023-10-05 19:29:14 +0700

    102305Tin Refactory code for user_template_list

--- js/test.js
+++
@@ -1,9 +0,0 @@
-import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
-
-const templatesRef = ref([])
-const originalTemplates = []
-const DEFAULT_ITEMS_PER_PAGE = 5
-const DEFAULT_PAGE = 1
-const templateTableTemplate = document.querySelector('template#template-table-template')?.innerHTML || ''
-let currentPage = DEFAULT_PAGE
-const filter = ref({})
--- js/user_template_management.js
+++ js/user_template_management.js
@@ -1,28 +1,28 @@
 import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
-const templatesRef = ref([])
-const originalTemplates = []
 const DEFAULT_ITEMS_PER_PAGE = 5
 const DEFAULT_PAGE = 1
-const templateRowsTemplate = document.querySelector('template#template-row-template')?.innerHTML || ''
-let currentPage = DEFAULT_PAGE
-const ratioSelectBox = document.querySelector('select#ratio-selectbox')
-const subjectSeclectBox = document.querySelector('select#subject-selectbox')
-const itemsPerPageSelectBox = document.querySelector('select#items-per-page')
-const prevPageBtn = document.querySelector('button#prev-page-btn')
-const nextPageBtn = document.querySelector('button#next-page-btn')
-const pageCountSection = document.querySelector('#page-count')
-const templateRows = createApp({
-    setup() {
-        return {
-            templates: templatesRef,
-            copyToClipboard
-        }
-    },
-    template: templateRowsTemplate
+const DEFAULT_RATIO_FILTER = 'all'
+const DEFAULT_SUBJECT_FILTER = 'all'
+const filteredTemplatesRef = ref([])
+const paginatedTemplatesRef = ref([])
+const templatesRef = ref([])
+const startTemplateIndexRef = ref(0)
+const endTemplateIndexRef = ref(0)
+const templateTableTemplate = document.querySelector('template#template-table-template')?.innerHTML || ''
+
+const filterRef = ref({
+    ratio: DEFAULT_RATIO_FILTER,
+    subject: DEFAULT_SUBJECT_FILTER
 })
 
-templateRows.mount("#template-rows")
+const paginationConfigRef = ref({
+    page: DEFAULT_PAGE,
+    itemsPerPage: DEFAULT_ITEMS_PER_PAGE,
+    startIndex: 0,
+    endIndex: 0,
+    itemCount: 0,
+})
 
 async function getTemplateData() {
     const res = await fetch('./get_template_data.php')
@@ -30,64 +30,10 @@ async function getTemplateData() {
     return await res.json()
 }
 
-getTemplateData().then(fetchedTemplates => {
-    originalTemplates.splice(0, originalTemplates.length)
-    originalTemplates.push(...fetchedTemplates)
-    const filter = getFilter()
-    templatesRef.value = filterTemplate(originalTemplates, filter)
-})
-
-
-ratioSelectBox.addEventListener('change', (e) => {
-    if (!e.target) { return }
-    // reset current page if change ratio
-    currentPage = DEFAULT_PAGE
-    handleFilterTemplate()
-})
-
-subjectSeclectBox.addEventListener('change', (e) => {
-    if (!e.target) { return }
-    // reset current page if change subject
-    currentPage = DEFAULT_PAGE
-    handleFilterTemplate()
-})
-
-itemsPerPageSelectBox.addEventListener('change', (e) => {
-    if (!e.target) { return }
-    handleFilterTemplate()
-})
-
-prevPageBtn.addEventListener('click', (e) => {
-    if (!e.target) { return }
-    if (currentPage > 1) {
-        currentPage--
-        handleFilterTemplate()
-    }
-})
-
-nextPageBtn.addEventListener('click', (e) => {
-    if (!e.target) { return }
-    currentPage++
-    handleFilterTemplate()
-})
-
-function handleFilterTemplate() {
-    const filter = getFilter()
-    const filteredTemplates = filterTemplate(originalTemplates, filter)
-    templatesRef.value = filteredTemplates
-}
-
-function getFilter() {
-    const ratio = ratioSelectBox.value
-    const subject = subjectSeclectBox.value
-    const itemsPerPage = Number(itemsPerPageSelectBox.value)
-    return { ratio, subject, itemsPerPage, page: currentPage }
-}
-
-function filterTemplate(templates, { ratio = 'all', subject = 'all', itemsPerPage, page }) {
+function filterTemplates(templates, { ratio = 'all', subject = 'all', itemsPerPage, page }) {
     const filteredTemplates = filterTemplateBySubject(filteredTemplatesByRatio(templates, ratio), subject)
 
-    return paginateTemplates(filteredTemplates, { itemsPerPage, page })
+    return filteredTemplates
 }
 
 function filteredTemplatesByRatio(templates, ratio = 'all') {
@@ -122,22 +68,103 @@ function filterTemplateBySubject(templates, subject = 'all') {
 }
 
 function paginateTemplates(templates, { itemsPerPage = DEFAULT_ITEMS_PER_PAGE, page = DEFAULT_PAGE }) {
-    const startIndex = (page - 1) * itemsPerPage
-    const endIndex = startIndex + itemsPerPage
-
-    if (endIndex >= templates.length) {
-        nextPageBtn.disabled = true
-    } else {
-        nextPageBtn.disabled = false
-    }
-
-    if (startIndex === 0) {
-        prevPageBtn.disabled = true
-    } else {
-        prevPageBtn.disabled = false
-    }
-
-    pageCountSection.innerHTML = `${startIndex + 1}-${endIndex < templates.length ? endIndex : templates.length} of ${templates.length}`
-
+    const { startIndex, endIndex } = getPageIndexes({ page, itemsPerPage })
     return templates.slice(startIndex, endIndex)
 }
+
+function getPageIndexes({ page = DEFAULT_PAGE, itemsPerPage = DEFAULT_ITEMS_PER_PAGE }) {
+    const startIndex = (page - 1) * itemsPerPage
+    const endIndex = startIndex + itemsPerPage
+    return { startIndex, endIndex }
+}
+
+function markChangeFilter() {
+    filteredTemplatesRef.value = filterTemplates(templatesRef.value, filterRef.value)
+}
+
+function markChangePaginationConfig() {
+    paginatedTemplatesRef.value = paginateTemplates(filteredTemplatesRef.value, paginationConfigRef.value)
+    let { startIndex, endIndex } = getPageIndexes(paginationConfigRef.value)
+    const newItemCount = filteredTemplatesRef.value.length
+    const { page, itemsPerPage } = paginationConfigRef.value
+
+    if (endIndex > newItemCount) {
+        endIndex = newItemCount
+    }
+
+    paginationConfigRef.value = {
+        startIndex,
+        endIndex,
+        itemCount: newItemCount,
+        itemsPerPage,
+        page,
+    }
+}
+
+function handleChangeItemsPerPage(e) {
+    const selectedItemsPerPage = Number(e.target.value)
+    paginationConfigRef.value.itemsPerPage = selectedItemsPerPage
+    paginationConfigRef.value.page = DEFAULT_PAGE
+    markChangePaginationConfig()
+}
+
+function handlePrevPage() {
+    if (paginationConfigRef.value.page > 1) {
+        paginationConfigRef.value.page--
+        markChangePaginationConfig()
+    }
+}
+
+function handleNextPage() {
+    const isEndPage = paginationConfigRef.value.endIndex >= paginationConfigRef.value.itemCount
+    if (!isEndPage) {
+        paginationConfigRef.value.page++
+        markChangePaginationConfig()
+    }
+}
+
+function handleChangeRatioFilter(e) {
+    const selectedRatio = String(e.target.value)
+    filterRef.value.ratio = selectedRatio
+    paginationConfigRef.value.page = DEFAULT_PAGE
+    markChangeFilter()
+    markChangePaginationConfig()
+}
+
+function handleChangeSubjectFilter(e) {
+    const selectedSubject = String(e.target.value)
+    filterRef.value.subject = selectedSubject
+    paginationConfigRef.value.page = DEFAULT_PAGE
+    markChangeFilter()
+    markChangePaginationConfig()
+}
+
+
+const templateRows = createApp({
+    setup() {
+        return {
+            filteredTemplates: filteredTemplatesRef,
+            templates: templatesRef,
+            filter: filterRef,
+            paginationConfig: paginationConfigRef,
+            paginatedTemplates: paginatedTemplatesRef,
+            copyToClipboard,
+            startTemplateIndex: startTemplateIndexRef,
+            endTemplateIndex: endTemplateIndexRef,
+            handleChangeItemsPerPage,
+            handleNextPage,
+            handlePrevPage,
+            handleChangeRatioFilter,
+            handleChangeSubjectFilter,
+        }
+    },
+    template: templateTableTemplate
+})
+
+templateRows.mount("#template-table")
+
+getTemplateData().then(fetchedTemplates => {
+    templatesRef.value = fetchedTemplates
+    markChangeFilter()
+    markChangePaginationConfig()
+})
