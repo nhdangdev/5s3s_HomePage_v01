360d8af00380415897213ee8f656748378d99150
Nhựt Đăng <nhutdangtran98@gmail.com>
2024-01-19 11:04:23 +0700

    fix splide

--- js/home_page_v01.js
+++ js/home_page_v01.js
@@ -33,51 +33,45 @@ var splideImg = new Splide(".splideImg", {
 splideImg.mount();
 
 // Splide AutoScroll
-  const splideAutoScrollRight = new Splide('.splideAutoScrollRight', {
-    type   : 'loop',
-    drag   : 'free',
-    focus  : 'center',
-    perPage: 3,
-    autoScroll: {
-      speed: 1,
-    },
-  });
-  splideAutoScrollRight.mount( );
+// window.splide.Extensions
+const splideAutoScrollRight = new Splide('.splideAutoScrollRight', {
+  type: 'loop',
+  drag: 'free',
+  focus: 'center',
+  perPage: 3,
+  autoScroll: {
+    speed: 1,
+  },
+  autoStart: true,
+});
+splideAutoScrollRight.mount(window.splide.Extensions);
 
-  const splideAutoScrollLeft = new Splide('.splideAutoScrollLeft', {
-    type   : 'loop',
-    drag   : 'free',
-    focus  : 'center',
-    perPage: 3,
-    autoScroll: {
-      speed: -1,
-    },
-  });
-  splideAutoScrollLeft.mount( );
+const splideAutoScrollLeft = new Splide('.splideAutoScrollLeft', {
+  type: 'loop',
+  drag: 'free',
+  focus: 'center',
+  perPage: 3,
+  autoScroll: {
+    speed: -1,
+  },
+  autoStart: true,
+});
+splideAutoScrollLeft.mount(window.splide.Extensions);
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   // Right to Left
-//   new Splide('.splideAutoScrollRight', {
-//     type: 'loop',
-//     drag: 'free',
-//     focus: 'center',
-//     perPage: 3,
-//     autoScroll: {
-//       speed: 1,
-//     },
-//   }).mount(window.splide.Extensions);
-  
+// Show/ hide text Ask
+function toggleTextAsk() {
+  var hiddenText = document.getElementById('hidden-text');
+  hiddenText.classList.toggle("hidden");
+  var toggleSvg = document.getElementById("toggle-svg");
+  var togglePath = document.getElementById("toggle-path");
 
-//   // Left to Right
-//   const splideAutoScrollLeft = new Splide('.splideAutoScrollLeft', {
-//     type: 'loop',
-//     drag: 'free',
-//     focus: 'center',
-//     perPage: 3,
-//     autoScroll: {
-//       speed: -1,
-//     },
-//   });
+  if (togglePath.getAttribute("d") === "M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z") {
+    togglePath.setAttribute("d", "M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z");
+  } else {
+    togglePath.setAttribute("d", "M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z");
+  }
+}
 
-//   splideAutoScrollLeft.mount();
-// });
+// Video
+const videoPlayer = document.querySelector('.video-player')
+initCustomVideoPlayer(videoPlayer)
