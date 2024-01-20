d830900fe20a9529d786d45a6395fc823d317b9f
Nhựt Đăng <nhutdangtran98@gmail.com>
2024-01-20 23:08:02 +0700

    fix bugs and reponsive mobile

--- js/home_page_v01.js
+++ js/home_page_v01.js
@@ -66,64 +66,7 @@ const splideAutoScrollLeft = new Splide('.splideAutoScrollLeft', {
 splideAutoScrollLeft.mount({ AutoScroll });
 // window.splide.Extensions
 
-// Show/ hide AskQuestion
-function toggleTextAsk() {
-  var hiddenText = document.getElementById('hidden-text');
-  hiddenText.classList.toggle("hidden");
-  var toggleSvg = document.getElementById("toggle-svg");
-  var togglePath = document.getElementById("toggle-path");
-
-  if (togglePath.getAttribute("d") === "M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z") {
-    togglePath.setAttribute("d", "M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z");
-  } else {
-    togglePath.setAttribute("d", "M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z");
-  }
-}
 
 // Video
 const videoPlayer = document.querySelector('.video-player')
 initCustomVideoPlayer(videoPlayer)
-
-// // Lấy phần tử feature
-// const feature = document.querySelector('.show-description');
-
-// // Gán sự kiện click cho feature
-// feature.addEventListener('click', () => {
-//   // Ẩn mô tả (desc) và chuyển màu văn bản và ảnh thành màu đen
-//   const desc = feature.querySelector('.ml-10');
-//   const img = feature.querySelector('img');
-//   desc.style.display = 'none';
-//   img.style.filter = 'grayscale(100%)';
-
-//   // Thay đổi nội dung HTML để chuyển màu văn bản và ảnh thành màu đen
-//   feature.innerHTML = `
-//     <div class="flex items-center mb-2">
-//       <div
-//         class="bg-black rounded-full hover:bg-[#F37B74] active:bg-[#F37B74] focus:outline-none focus:ring focus:ring-[#F37B74] cursor-pointer"
-//       >
-//         <img src="../asset/img/icon/lock-icon.svg" alt="" style="filter: grayscale(100%);" />
-//       </div>
-//       <h3 class="title-1-text-bold text-[#000000] ml-2">
-//         Menu Design Customization:
-//       </h3>
-//     </div>`;
-// });
-
-// SHow desc features
-const bgFeatures = document.querySelectorAll('.bg-features');
-const descFeatures = document.querySelectorAll('.desc-features');
-
-bgFeatures.forEach(descFeature => {
-  descFeature.addEventListener('click', () => {
-    document.querySelector('.active')?.classList.remove('active');
-    descFeature.classList.add('active');
-  });
-})
-
-descFeatures.forEach(descFeature => {
-  descFeature.addEventListener('click', () => {
-    document.querySelector('.hiddene')?.classList.add('hiddene');
-    descFeature.classList.remove('hiddene');
-  });
-})
-
